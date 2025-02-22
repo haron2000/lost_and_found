d5eddd9f48084a2030089885ce75594c312b028a
Starnoh John <130779431+Starnoh254@users.noreply.github.com>
2025-02-22 14:56:14 +0300

    `Updated templates and settings for Lost and Found ID System`

--- lost_and_found/settings.py
+++ lost_and_found/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)0pwkc=iht^7-co-*wqeis^_q+3(2kltmsdn3e3i*)e&e-y_dz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.0.29']
 
 
 # Application definition
